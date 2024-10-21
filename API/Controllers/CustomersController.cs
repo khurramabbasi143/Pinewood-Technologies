275f2cbf834afc85ab139189953973099ead8f1d
Khurram Abbasi <khurram.abbasi143@outlook.com>
2024-10-21 22:23:32 +0100

    all project

---
+++ API/Controllers/CustomersController.cs
@@ -0,0 +1,109 @@
+﻿using API.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace API.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private static List<Customer> customers = new List<Customer>();
+
+        public CustomersController()
+        {
+            if (customers.Count<1)
+            {
+                // Initializing with some data
+                customers.Add(new Customer { Id = 1, Name = "John Doe1", Email = "john@example.com", Phone = "123456789" });
+                customers.Add(new Customer { Id = 2, Name = "Jane Doe2", Email = "jane@example.com", Phone = "987654321" });
+                customers.Add(new Customer { Id = 3, Name = "Jane Doe3", Email = "jane@example.com", Phone = "987654321" });
+            }
+        }
+
+        #region Get
+
+        // GET api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(customers.ToList());
+        }
+
+        // GET api/customers/5
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+
+        #endregion
+
+        #region Post
+
+        // POST api/customers
+        public IHttpActionResult CreateCustomer(Customer customer)
+        {
+            var existingCustomer = customers.FirstOrDefault(c => c.Id == customer.Id);
+            if (existingCustomer != null)
+            {
+                return BadRequest("Customer with this ID already exists.");
+            }
+            customers.Add(customer);
+            return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
+        }
+
+
+        #endregion
+
+
+        #region Put
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult EditCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingCustomer = customers.FirstOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+            existingCustomer.Name = customer.Name;
+            existingCustomer.Email = customer.Email;
+            existingCustomer.Phone = customer.Phone;
+            return Ok(existingCustomer);
+        }
+
+
+        #endregion
+
+
+        #region Delete
+        // DELETE api/customers/5
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            customers.Remove(customer);
+            return Ok();
+        }
+        #endregion
+
+
+    }
+}
---
+++ WebUI/Controllers/CustomersController.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _localHostUrl= "https://localhost:44395/api/Customers";
+
+        public CustomersController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5000/api/");
+        }
+
+        #region Index
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var response = _httpClient.GetAsync(_localHostUrl).Result;
+            var customers = JsonConvert.DeserializeObject<List<Customer>>(response.Content.ReadAsStringAsync().Result);
+            return View(customers);
+        }
+
+        #endregion
+
+
+        #region Create
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(customer);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync(_localHostUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(customer);
+        }
+
+        #endregion
+
+        #region Delete
+
+        // DELETE: Customers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var response = _httpClient.GetAsync(_localHostUrl + "/" + id).Result;
+            var customer = JsonConvert.DeserializeObject<Customer>(response.Content.ReadAsStringAsync().Result);
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var response = _httpClient.DeleteAsync(_localHostUrl + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["Message"] = "Record deleted successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        #endregion
+
+        #region Edit
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var response = _httpClient.GetAsync(_localHostUrl + "/" + id).Result;
+            var customer = JsonConvert.DeserializeObject<Customer>(response.Content.ReadAsStringAsync().Result);
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(customer);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync(_localHostUrl + "/" + customer.Id, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(customer);
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
